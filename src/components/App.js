3d60c5ba01f75f7fd99bce2b5599b83570bf7354
lindsayMecher <lindsaymecher@gmail.com>
2020-11-18 15:39:38 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+const PETS_ENDPOINT = `/api/pets`;
 
 class App extends React.Component {
   constructor() {
@@ -9,13 +10,80 @@ class App extends React.Component {
 
     this.state = {
       pets: [],
+      filteredPets: [],
       filters: {
         type: 'all'
       }
     }
   }
 
+  constructURL = () => {
+    const { type } = this.state.filters
+    let finalURL = ""
+    if (type === "all") {
+      finalURL = PETS_ENDPOINT
+    } else {
+      const queryString = `?type=${type}`
+      finalURL = PETS_ENDPOINT + queryString
+    }
+    return finalURL
+  }
+
+  componentDidMount(){
+    fetch(PETS_ENDPOINT)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          pets: data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  onChangeType = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: e.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    fetch(this.constructURL())
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          pets: data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  onAdoptPet = (id) => {
+    console.log("adopting", id)
+    // find pet with this id
+    // update isAdopted to true
+    // set state
+    const petToUpdate = this.state.pets.find(pet => pet.id === id)
+    const idx = this.state.pets.indexOf(petToUpdate)
+    const newState = {
+      pets: [
+        ...this.state.pets.slice(0, idx),
+        {
+          ...this.state.pets[idx],
+          isAdopted: true
+        },
+        ...this.state.pets.slice(idx + 1)
+      ]
+      
+    }
+    this.setState(newState)
+  }
+
   render() {
+    console.log(this.state.pets)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +92,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters pets={this.state.pets} filters={this.state.filters} onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
